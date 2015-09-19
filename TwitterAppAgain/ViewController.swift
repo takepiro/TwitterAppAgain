a1e68d18a0d7a5a198f54b3fbd0432d975410789
Takehiro <ishii@dopa-service.jp>
2015-09-19 15:38:50 +0900

    Initial Commit

---
+++ TwitterAppAgain/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TwitterAppAgain
+//
+//  Created by Takehiro Ishii on 2015/09/19.
+//  Copyright (c) 2015年 Takehiro Ishii. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
